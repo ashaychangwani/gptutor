8cbdc991053a9dbf675b7a317da28dd2e5902e37
farhan0167 <afi225@nyu.edu>
2023-02-19 07:43:27 -0500

    discordbot repo- code update

--- discordBot/bot.py
+++ discordBot/bot.py
@@ -6,7 +6,7 @@ import requests
 #from dotenv import load_dotenv
 
 #load_dotenv()
-TOKEN = "ask me"
+TOKEN = "MTA3NjYyNjAxMjM5NTI3NDI2MA.GizPcc.bBgYpuLlm8CdwVMBR1zSh6g6LplTKr0X6GpWwI"
 GUILD = "1076626797061488640"
 API_SERVER = "http://10.18.210.197:8001"
 
@@ -55,9 +55,14 @@ def dealWithMSG(query):
 def processQuery(messages):
     processedMessages = ""
     for i in range(len(messages)-1,0,-1):
+        print(messages[i])
         if i == len(messages)-1 or messages[i].content.startswith('!query'): 
-            processedMessages += 'Q: '+messages[i].content +"\n"
-        elif messages[i].author.name == 'nyuHackBot':
+            processedMessages += 'Q: '+ removeExclQuery(messages[i].content) +"\n"
+        elif messages[i].author.name == 'nyuHackBot' or messages[i].author.name =='Shubh587':
+            if messages[i].content.startswith('Hello') and messages[i].author.name == 'nyuHackBot':
+                continue
+            if messages[i].content == "I don't know.":
+                continue
             processedMessages += 'A: '+messages[i].content + "\n"
     return processedMessages 
 
@@ -66,10 +71,12 @@ def get_response(query, history = None):
     data = {
         "message": query,
     }
+    print('payload of POST',data)
     if history is not None:
         data['history'] = history
     x = requests.post(API_SERVER+"/query", data = data)
     response = x.json()
+    print('response of POST',response)
     if(x.status_code == 200):
         return response['text']
     else:
@@ -93,7 +100,7 @@ def get_response_put(history):
 async def on_message(message):
     if message.author == client.user:
         return
-    print(message.author.roles[1].name)
+    #print('Professor' in [role.name for role in message.author.roles])
     if message.content.startswith('!query') and message.channel.name == 'general':
         #print(message)
         print("channel")
@@ -124,7 +131,7 @@ async def on_message(message):
             queryToProcess = [message async for message in thread.history()]
             formattedHist = processQuery(queryToProcess)
             response = get_response_put( formattedHist )
-            await thread.send(response)
+            await message.add_reaction("👍")
         #for thumbsdown cases
         elif "\U0001F44E" in message.content:
             print("This message contains a thumbs down emoji!")
